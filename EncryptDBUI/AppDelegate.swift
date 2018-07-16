7a42ee92591f07aea236f3c1a31cc3ed42fb220b
yuqiang <1054572107@qq.com>
2018-07-16 09:47:06 +0800

    Initial Commit

---
+++ EncryptDBUI/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  EncryptDBUI
+//
+//  Created by sungrow on 2018/7/16.
+//  Copyright © 2018年 sungrow. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ EncryptDBUI/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  EncryptDBUI
+//
+//  Created by sungrow on 2018/7/16.
+//  Copyright © 2018年 sungrow. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
