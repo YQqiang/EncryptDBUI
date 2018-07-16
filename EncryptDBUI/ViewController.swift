6e098f316a6a7fc1280657adf7923d22c119f10b
yuqiang <1054572107@qq.com>
2018-07-16 11:39:54 +0800

    加解密数据库

--- EncryptDBUI/ViewController.swift
+++ EncryptDBUI/ViewController.swift
@@ -9,8 +9,11 @@
 import Cocoa
 
 class ViewController: NSViewController {
+    
+    fileprivate var currentFileModel: YQFileModel?
 
     @IBOutlet weak var dragDropView: YQDragDropView!
+    @IBOutlet weak var encryptKeyTF: NSTextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         dragDropView.delegate = self
@@ -22,22 +25,67 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
+    
+    @IBAction func encrypt(_ sender: NSButton) {
+        guard let model = currentFileModel else {
+            return
+        }
+        let isSuccess = FMEncryptHelper.encryptDatabase(model.filePath, encryptKey: encryptKeyTF.stringValue)
+        if isSuccess {
+            dragDropView.text = "加密成功"
+        } else {
+            dragDropView.text = "加密失败"
+        }
+    }
+    
+    @IBAction func decrypt(_ sender: NSButton) {
+        guard let model = currentFileModel else {
+            return
+        }
+        let isSuccess = FMEncryptHelper.unEncryptDatabase(model.filePath, encryptKey: encryptKeyTF.stringValue)
+        if isSuccess {
+            dragDropView.text = "解密成功"
+        } else {
+            dragDropView.text = "解密失败"
+        }
+    }
+}
+
+// MARK: - private func
+extension ViewController {
+    fileprivate func reloadDefaultData() {
+        currentFileModel = nil
+        dragDropView.text = "拖拽数据库到这里"
+        dragDropView.image = NSImage(named: NSImage.Name(rawValue: "addDB"))
+    }
 }
 
 extension ViewController: YQDragDropViewDelegate {
     func draggingEntered(_ dragDropView: YQDragDropView) {
-        dragDropView.text = "释放 完成拖拽"
+        if let _ = currentFileModel {
+            return
+        }
+        dragDropView.text = "释放完成拖拽"
     }
     
     func draggingExited(_ dragDropView: YQDragDropView) {
+        if let _ = currentFileModel {
+            return
+        }
         dragDropView.text = "拖拽数据库到这里"
     }
     
     func draggingFileAccept(_ dragDropView: YQDragDropView, files: [String]) {
-        files.forEach { (pathStr) in
-            let fileModel = YQFileModel(filePath: pathStr)
-            print("\(fileModel.fileName)")
+        if let _ = currentFileModel {
+            return
         }
+        if files.count > 1 {
+            dragDropView.text = "拖拽数据库到这里"
+            return
+        }
+        currentFileModel = YQFileModel(filePath: files.first!)
+        dragDropView.text = currentFileModel?.fileName
+        dragDropView.image = NSImage(named: NSImage.Name(rawValue: "dbIcon"))
     }
 }
 
