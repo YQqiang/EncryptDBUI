00b23f1e4c9b9465d1d88ef2fbeb56e48d015bca
yuqiang <1054572107@qq.com>
2020-02-28 13:22:43 +0800

    remove Pods

--- Pods/Target Support Files/Pods-EncryptDBUI/Pods-EncryptDBUI-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_EncryptDBUI : NSObject
-@end
-@implementation PodsDummy_Pods_EncryptDBUI
-@end
--- Pods/Target Support Files/Pods-EncryptDBUITests/Pods-EncryptDBUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_EncryptDBUITests : NSObject
-@end
-@implementation PodsDummy_Pods_EncryptDBUITests
-@end
